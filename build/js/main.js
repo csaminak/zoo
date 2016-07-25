216ca69a59fcab8d21565fdc5253b05b0b7cbafc
Samina Chhatwal <csaminak@gmail.com>
2016-07-25 17:10:03 -0400

    created reptile and mammal constructors

--- build/js/main.js
+++ build/js/main.js
@@ -14,7 +14,25 @@
     };
 
 
-    
+})(window.zoo);
+
+(function(ns){
+    'use strict';
+    window.zoo = ns = (ns || {});
+
+    ns.Reptile = function Reptile(name) {
+        window.zoo.Animal.call(this, name);
+    };
+    ns.Reptile.prototype = Object.create(window.zoo.Animal.prototype);
+
+
+
+    ns.Mammal = function Mammal(name) {
+        window.zoo.Animal.call(this, name);
+    };
+    ns.Mammal.prototype = Object.create(window.zoo.Animal.prototype);
+
+
 
 
 })(window.zoo);
--- src/js/species.js
+++ src/js/species.js
@@ -2,7 +2,19 @@
     'use strict';
     window.zoo = ns = (ns || {});
 
-    
+    ns.Reptile = function Reptile(name) {
+        window.zoo.Animal.call(this, name);
+    };
+    ns.Reptile.prototype = Object.create(window.zoo.Animal.prototype);
+
+
+
+    ns.Mammal = function Mammal(name) {
+        window.zoo.Animal.call(this, name);
+    };
+    ns.Mammal.prototype = Object.create(window.zoo.Animal.prototype);
+
+
 
 
 })(window.zoo);
