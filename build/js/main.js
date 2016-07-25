3f64c3644c2bc9da7a7bd20314155f63a97e76fc
Samina Chhatwal <csaminak@gmail.com>
2016-07-25 16:13:32 -0400

    created animal constructor

--- build/js/main.js
+++ build/js/main.js
@@ -1,12 +1,18 @@
-(function(){
+(function(ns){
     'use strict';
+    window.zoo = ns = (ns || {});
+
+
+    ns.Animal = function Animal(name){
+        // var birthtime = Date.now();
+
+        this.name = name;
+        this.dob = new Date();
+        // this.age = Date.now() - birthtime;
+    };
 
 
 
-
-
-
-
-})();
+})(window.zoo);
 
 //# sourceMappingURL=main.js.map
--- src/js/zoo.js
+++ src/js/zoo.js
@@ -1,10 +1,16 @@
-(function(){
+(function(ns){
     'use strict';
+    window.zoo = ns = (ns || {});
+
+
+    ns.Animal = function Animal(name){
+        // var birthtime = Date.now();
+
+        this.name = name;
+        this.dob = new Date();
+        // this.age = Date.now() - birthtime;
+    };
 
 
 
-
-
-
-
-})();
+})(window.zoo);
