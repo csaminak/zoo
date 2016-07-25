fa84558816032677f8dcf2c0e94fcff9e913b10e
Samina Chhatwal <csaminak@gmail.com>
2016-07-25 16:13:15 -0400

    created name and birthdate test

--- test/specs/species.js
+++ test/specs/species.js
@@ -1,10 +1,19 @@
 (function(){
     'use strict';
 
-    
+    var assert = window.chai.assert;
 
+    suite('tests for zoo', function() {
 
-
+        test('animal constructor has name and dob', function() {
+            var turtle = new window.zoo.Animal('Lily');
+            var date = new Date();
+            assert.strictEqual(turtle.name, 'Lily', 'species has a name!');
+            assert.strictEqual(turtle.dob.getMonth(), date.getMonth(), 'birth month matches');
+            assert.strictEqual(turtle.dob.getYear(), date.getYear(), 'birth year matches');
+            assert.strictEqual(turtle.dob.getDate(), date.getDate(), 'birth date matches');
+        });
+    });
 
 
 })();
