e3111bda0ced431754646cd4c499aba5a4e36621
Samina Chhatwal <csaminak@gmail.com>
2016-07-25 17:09:17 -0400

    created species inheritance test

--- test/specs/species.js
+++ test/specs/species.js
@@ -3,7 +3,12 @@
 
     var assert = window.chai.assert;
 
-    suite('', function(){
+    suite('tests for species constructor', function(){
+
+        test('species constructor inherits name trait from Animal', function(){
+            var turtle = new window.zoo.Reptile('Chelsea');
+            assert.strictEqual(turtle.name, 'Chelsea', 'turtle has a name');
+        });
 
 
 
