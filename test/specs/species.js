d568ec8b3f1853aab54ccd5510e747d16ecd19c0
Samina Chhatwal <csaminak@gmail.com>
2016-07-25 17:26:13 -0400

    changed test to reflect specfic species

--- test/specs/species.js
+++ test/specs/species.js
@@ -3,12 +3,17 @@
 
     var assert = window.chai.assert;
 
-    suite('tests for species constructor', function(){
+    suite('tests for species constructor', function() {
 
-        test('species constructor inherits name trait from Animal', function(){
-            var turtle = new window.zoo.Reptile('Chelsea');
-            assert.strictEqual(turtle.name, 'Chelsea', 'turtle has a name');
+        test('species constructor inherits name trait from Animal', function() {
+            var poodle = new window.zoo.Dog('Chelsea');
+            assert.isObject(poodle, 'poodle is an object')
+            assert.strictEqual(poodle.name, 'Chelsea', 'poodle has a name');
         });
+        // test('mammal constructor can give birth', function() {
+        //     var dog = new window.zoo.Mammal('Martha');
+        //     assert.strictEqual(dog.birth, true, 'a dog can give birth');
+        // });
 
 
 
