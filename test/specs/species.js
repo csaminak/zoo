6993793c809c28366a19b4d90f5d224b65e1d0fb
Samina Chhatwal <csaminak@gmail.com>
2016-07-25 17:45:14 -0400

    updated src file paths and test to take both species into account

--- test/specs/species.js
+++ test/specs/species.js
@@ -5,11 +5,15 @@
 
     suite('tests for species constructor', function() {
 
-        test('species constructor inherits name trait from Animal', function() {
+        test('species constructors inherits name trait from Animal', function() {
             var poodle = new window.zoo.Dog('Chelsea');
-            assert.isObject(poodle, 'poodle is an object')
-            assert.strictEqual(poodle.name, 'Chelsea', 'poodle has a name');
+            var parrot = new window.zoo.Bird('Polly');
+            assert.isObject(poodle, 'poodle is a Dog object');
+            assert.strictEqual(poodle.name, 'Chelsea', 'the poodle has a name');
+            assert.isObject(parrot, 'parrot is a Bird object');
+            assert.strictEqual(parrot.name, 'Polly', 'the parrot has a name');
         });
+
         // test('mammal constructor can give birth', function() {
         //     var dog = new window.zoo.Mammal('Martha');
         //     assert.strictEqual(dog.birth, true, 'a dog can give birth');
